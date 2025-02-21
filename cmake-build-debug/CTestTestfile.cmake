78fa81bb8f88dc3248570ac73bfd522d298e6d8a
simichanga <siminischi1234@gmail.com>
2025-02-21 19:05:01 +0200

    separated unit test logic into subfiles

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/simi/CLionProjects/linear-algebra-libcpp
+# Build directory: /home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ cmake-build-debug/tests/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/simi/CLionProjects/linear-algebra-libcpp/tests
+# Build directory: /home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_block_matrix_test_construction]=] "/home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug/tests/test_block_matrix_test_construction")
+set_tests_properties([=[test_block_matrix_test_construction]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;17;add_test;/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;0;")
+add_test([=[test_block_matrix_test_element_access]=] "/home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug/tests/test_block_matrix_test_element_access")
+set_tests_properties([=[test_block_matrix_test_element_access]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;17;add_test;/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;0;")
+add_test([=[test_block_matrix_test_initializer_list]=] "/home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug/tests/test_block_matrix_test_initializer_list")
+set_tests_properties([=[test_block_matrix_test_initializer_list]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;17;add_test;/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;0;")
+add_test([=[test_block_matrix_test_matrix_operations]=] "/home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug/tests/test_block_matrix_test_matrix_operations")
+set_tests_properties([=[test_block_matrix_test_matrix_operations]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;17;add_test;/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;0;")
+add_test([=[test_static_vector_test_static_vector]=] "/home/simi/CLionProjects/linear-algebra-libcpp/cmake-build-debug/tests/test_static_vector_test_static_vector")
+set_tests_properties([=[test_static_vector_test_static_vector]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;17;add_test;/home/simi/CLionProjects/linear-algebra-libcpp/tests/CMakeLists.txt;0;")
